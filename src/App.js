b05503aaaa3bdb3d071edf2653285df355d8faf9
Joel Christensen <christensenjo14@gmail.com>
2023-01-25 14:23:48 -0700

    Adding construction notice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import construction from './construction.svg';
 
 import Container from 'react-bootstrap/Container';
 import HeaderNav from './components/HeaderNav/HeaderNav.js';
 
 function App() {
+  const patientStyle = (
+    {
+      paddingTop: '15vh',
+      textAlign: 'center',
+    }
+  );
+  
   return (
     <Container className="App">
       <HeaderNav></HeaderNav>
@@ -14,6 +22,11 @@ function App() {
       {/* Info */}
       {/* Contact */}
       {/* FooterBar */}
+
+      <div style={patientStyle}>
+        <img src={construction} alt="Construction icon" />
+        <h1>Please be patient, this project is under construction...</h1>
+      </div>
     </Container>
   );
 }
