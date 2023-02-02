37d47241335d913995d3bb385415b37e8ada7e66
Joel Christensen <christensenjo14@gmail.com>
2023-02-01 17:37:55 -0700

    Form layout complete; now need styles

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HeaderNav from './components/HeaderNav/HeaderNav.js';
 import Hero from './components/Hero/Hero.js';
 import LogoDisplay from './components/LogoDisplay/LogoDisplay.js';
 import Info from './components/Info/Info.js'
-// import ContactForm from './components/ContactForm/ContactForm';
+import ContactForm from './components/ContactForm/ContactForm';
 
 function App() {
   const patientStyle = (
@@ -43,7 +43,7 @@ function App() {
           align="left"
         >
         </Info>
-        {/* <ContactForm></ContactForm> */}
+        <ContactForm></ContactForm>
         {/* FooterBar */}
 
         <div style={patientStyle}>
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,46 +1,51 @@
 import React from 'react';
 import './ContactForm.css';
-import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Stack from 'react-bootstrap/Stack';
-import ContactBlock from './ContactBlock/ContactBlock';
-
+import email from './email.svg';
+import phone from './phone.svg';
+import location from './location.svg';
 
 function ContactForm(props){
     return (
-        <Container>
-            <Row>
-                <Col>
-                    <Stack>
-                        <ContactBlock
-                            title="Email me:"
-                            content="christensenjo14@gmail.com"
-                            img="email"
-                        >
-                        </ContactBlock>
-                        <ContactBlock
-                            title="Call me:"
-                            content="+1-208-749-7563"
-                            img="phone"
-                        >
-                        </ContactBlock>
-                        <ContactBlock
-                            title="Homebase:"
-                            content="Logan, UT, USA"
-                            img="location"
-                        >
-                        </ContactBlock>
+        <Row md="12" className="Contact">
+            <Col md="2">
+                <Stack className="ContactIcons" gap={5}>
+                    <img src={email} alt="Email icon" />
+                    <img src={phone} alt="Phone icon" />
+                    <img src={location} alt="Location icon" />
+                </Stack>
+            </Col>
+            <Col md="3">
+                <Stack gap={5} className="ContactInfo">
+                        <p>Email me: <br /><strong>christensenjo14@gmail.com</strong></p>
+                        <p>Call me: <br /><strong>+1-208-749-7563</strong></p>
+                        <p>Homebase: <br /><strong>Logan, UT, USA</strong></p>
+                </Stack>
+            </Col>
+            <Col md="7" className="ContactFormCol">
+                <Form>
+                    <Stack gap={4}>
+                        <Row>
+                            <Form.Group as={Col} controlId="formFullName">
+                                <Form.Control size="lg" type="text" placeholder="Full Name" className="FormControl" />
+                            </Form.Group>
+                            <Form.Group as={Col} controlId="formEmail">
+                                <Form.Control size="lg" type="email" placeholder="Email" className="FormControl" />
+                            </Form.Group>
+                        </Row>
+                        <Form.Group>
+                            <Form.Control size="lg" type="text" placeholder="Subject" className="FormControl" />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Control size="md" as="textarea" rows="7" placeholder="Message" className="FormControl" />
+                        </Form.Group>
                     </Stack>
-                </Col>
-                <Col>
-                    <Form>
-                        <p>this is some teztx for the second col</p>
-                    </Form>
-                </Col>
-            </Row>
-        </Container>
+                </Form>
+            </Col>
+        </Row>
     );
 }
 
